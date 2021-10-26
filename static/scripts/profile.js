0e2c605bd1d7878c6d38739d76c4071e2329ca18
rambhumukhia <email@masaischool.com>
2021-10-26 12:33:06 +0530

    static file added

---
+++ static/scripts/profile.js
@@ -0,0 +1,15 @@
+function basic_profile() {
+    window.location.href = "/profile/basic-details";
+}
+
+function holding_stocks() {
+    window.location.href = "/profile/holding-stocks";
+}
+
+function change_password() {
+    window.location.href = "/profile/change-password";
+}
+
+function logout() {
+    window.location.href = "/profile/logout";
+}
